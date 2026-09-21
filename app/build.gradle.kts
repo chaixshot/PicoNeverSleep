8479955efa81b71fad54ad00baa58e65f712332b
chaixshot <chai5142xshot@gmail.com>
2026-09-21 17:40:35 +0700

    pump version 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.hamer.piconeversleep"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
